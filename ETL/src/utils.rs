c2c22aaf871d51872cb93fdf70422bad87efe45b
Jayslen <jayslen134@gmail.com>
2026-08-18 20:14:11 -0400

    update: header string's trim

--- ETL/src/utils.rs
+++ ETL/src/utils.rs
@@ -6,7 +6,7 @@ pub fn string_to_vec(value: Result<String, Error>) -> Result<Vec<String>, Error>
     let v = value?;
     let list = v
         .split(',')
-        .map(|s| s.to_string().replace("\n", ""))
+        .map(|s| s.to_string().trim().replace("\n", ""))
         .collect();
     Ok(list)
 }
